14d11651d9c21d33873b211e7d8dc0f964dfeec2
HamelA <40329125+HamelA@users.noreply.github.com>
2020-03-26 17:07:33 -0700

    Hovering over buttons
    Buttons change size when the mouse hovers over them

---
+++ IndependentStudy/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    private bool mouseHovering;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        mouseHovering = true;
+        ChangeTextScale(1.5f);
+    }
+ 
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        mouseHovering = false;
+        ChangeTextScale(1f);
+    }
+
+    private void ChangeTextScale(float newScale)
+    {
+        this.GetComponent<RectTransform>().localScale = new Vector3(newScale, newScale, newScale);
+    }
+}
---
+++ IndependentStudy/Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MenuHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    private bool mouse_over;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mouse_over)
+         {
+             Debug.Log("Mouse Over");
+         }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+     {
+         mouse_over = true;
+         Debug.Log("Mouse enter");
+     }
+ 
+     public void OnPointerExit(PointerEventData eventData)
+     {
+         mouse_over = false;
+         Debug.Log("Mouse exit");
+     }
+}
